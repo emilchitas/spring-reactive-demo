94f9a2d976ce26017b1783bcdac5608f4730f223
Emil Chitas <emil.chitas@dynatrace.com>
2021-11-06 19:01:56 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+# SPRING BOOT - REACTIVE 
+
+
+Small project to try out reactive  
---
+++ src/test/java/at/chitas/springreactivedemo/SpringReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package at.chitas.springreactivedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
